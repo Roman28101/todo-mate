f243527bceef29547efe2dbb0ab94b177775d692
Roman28101 <r.kovalenko28101@gmail.com>
2023-06-15 13:07:51 +0800

    Add urls.py into app

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("content",)
+    list_filter = ("task_status", "date")
+
+
+admin.site.register(Tag)
